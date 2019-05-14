68068488504263a0b33a4ec85b63c84465888f3a
Xavier Godbout <xaviergodbout@MacBook-Pro-de-Xavier-10.local>
2019-05-13 20:51:09 -0400

    Removed duplicated directory

--- tp2_blogue/mdp.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-
-    echo password_hash("admin", PASSWORD_DEFAULT) . "<br>";
-    echo password_hash("123456", PASSWORD_DEFAULT) . "<br>";
-    echo password_hash("123456", PASSWORD_DEFAULT);
-?>
